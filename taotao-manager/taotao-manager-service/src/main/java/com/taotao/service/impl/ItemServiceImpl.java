0b84f313fc8d9297cf7365428a73e45352189211
Yuan Lunchuan <799652013@qq.com>
2017-02-24 09:49:58 +0800

    1, update service pom.

--- taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -7,14 +7,13 @@ import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
 
-@Service(value = "itemService")
+@Service
 public class ItemServiceImpl implements ItemService {
-	//@Autowired
-	//private TbItemMapper itemMapper;
+	@Autowired
+	private TbItemMapper itemMapper;
 
 	@Override
 	public TbItem getItemById(long itemId) {
-		System.out.println("-------line 17");
-		return new TbItem();
+		return itemMapper.selectByPrimaryKey(itemId);
 	}
 }
--- taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -8,21 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.mapper.TbItemMapper;
 import com.taotao.pojo.TbItem;
 import com.taotao.service.ItemService;
+import com.taotao.service.impl.ItemServiceImpl;
 
 @Controller
 @RequestMapping("/")
 public class ItemController {
 	@Autowired
-	@Qualifier("itemService")
 	private ItemService itemService;
-
+	
 	@RequestMapping(value="item/{itemId}", method=RequestMethod.GET)
 	@ResponseBody
 	public TbItem getTbItemById(@PathVariable(value="itemId") Long itemId){
 		System.out.println("------itemId: "+itemId);
+		System.out.println("---------type: "+ (itemService instanceof ItemServiceImpl));
+		System.out.println("--------itemService: "+itemService);
 		return itemService.getItemById(itemId);
+	}
+	
+	public static void main(String[] args) {
+		new ItemServiceImpl().getItemById(33333L);
 	}
 	
 	@RequestMapping(value="users", method=RequestMethod.GET)
